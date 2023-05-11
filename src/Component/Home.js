5198819fc90d8908a20bae6349de84f78384dd41
Romil Wadhwani <romil.wadhwani@yash.com>
2023-05-11 17:12:01 +0530

    Updated Home component

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import NavBar from "./Navbar";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import ImageStepper from "./ImageStepper";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <Box>
-      <NavBar posittion={'fixed'}/>
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        flexDirection: "column",
+      }}
+    >
+      <NavBar posittion={"fixed"} />
       {/* <Typography
         sx={{
           m: 3,
@@ -17,7 +24,39 @@ export default function Home() {
       >
         Welcome to Kids Corner School Dhamnod
       </Typography> */}
-      <ImageStepper/>
+      <ImageStepper />
+      <Box sx={{ flexGrow: 1, m: 3 }}>
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12}>
+            <Typography
+              mb={2}
+              variant="h5"
+              fontWeight={"bold"}
+              color={"#008080"}
+            >
+              Dear Parents and Students,
+            </Typography>
+            <Typography variant="body1" fontSize={20}>
+              Welcome to Kids Corner, a school dedicated to providing the
+              highest quality education to children from diverse backgrounds.
+              Our mission is to create a safe and supportive environment that
+              fosters the growth and development of each student academically,
+              socially, and emotionally.
+            </Typography>
+            <br/>
+            <Typography variant="body1" fontSize={20}>
+              At Kids Corner, we believe that every child is unique and has
+              their own strengths, interests, and learning style. Our curriculum
+              is designed to cater to individual needs and abilities, with a
+              focus on inquiry-based learning, critical thinking, and
+              creativity. We also prioritize the development of essential life
+              skills such as communication, collaboration, and problem-solving,
+              which are essential for success in the 21st century.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Footer/>
     </Box>
   );
 }
